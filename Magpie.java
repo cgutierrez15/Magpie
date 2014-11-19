a2903187118a538da2911ea2144ac19d6111e2d3
cgutierrez15 <christiangutierrez@christiansair2.punahou.edu>
2014-11-18 15:16:46 -1000

    Activity 5
    Includes activities 1-5, including how to deal with present and past
    sentences (and returning a random response for phrases that it has not
    been programmed to handle).

--- Magpie.java
+++ Magpie.java
@@ -1,12 +1,14 @@
+import java.util.Random;
+
 /**
  * A program to carry on conversations with a human user.
- * This is the initial version that:  
- * <ul><li>
- *       Uses indexOf to find strings
- * </li><li>
- *       Handles responding to simple words and phrases 
- * </li></ul>
- * This version uses a nested if to handle default responses.
+ * This version:
+ *<ul><li>
+ *   Uses advanced search for keywords 
+ *</li><li>
+ *   Will transform statements as well as react to keywords
+ *</li></ul>
+ * This version uses an array to hold the default responses.
  * @author Laurie White
  * @version April 2012
  */
@@ -15,7 +17,7 @@ public class Magpie
  /**
   * Get a default greeting  
   * @return a greeting
-  */
+  */ 
  public String getGreeting()
  {
   return "Hello, let's talk.";
@@ -31,101 +33,400 @@ public class Magpie
  public String getResponse(String statement)
  {
   String response = "";
-  if (statement.indexOf("no") >= 0)
+  if (statement.trim().length() == 0)
+  {
+   response = "Say something to me!";
+  }
+  else if (findKeyword(statement, "no") >= 0)
   {
    response = "Why so negative?";
   }
-  if (statement.indexOf("mother") >= 0
-    || statement.indexOf("father") >= 0
-    || statement.indexOf("sister") >= 0
-    || statement.indexOf("brother") >= 0)
+
+  else if (findKeyword(statement, "mother") >= 0
+    || findKeyword(statement, "father") >= 0
+    || findKeyword(statement, "sister") >= 0
+    || findKeyword(statement, "brother") >= 0)
   {
    response = "Tell me more about your family.";
   }
-   if (statement.indexOf("dog") >= 0
+
+   else if (statement.indexOf("dog") >= 0
     || statement.indexOf("cat") >= 0
     || statement.indexOf("fish") >= 0
     || statement.indexOf("bird") >= 0)
   {
    response = "Tell me more about your pets.";
   }
-    if (statement.indexOf("car") >= 0
+    else if (statement.indexOf("car") >= 0
     || statement.indexOf("drive") >= 0
     || statement.indexOf("tire") >= 0
     || statement.indexOf("crash") >= 0)
   {
    response = "Tell me more about your car.";
   }
-    if (statement.indexOf("camera") >= 0
+   else if (statement.indexOf("camera") >= 0
     || statement.indexOf("photo") >= 0
     || statement.indexOf("picture") >= 0
     || statement.indexOf("image") >= 0)
   {
    response = "Tell me more about your photography.";
   }
-    if (statement.indexOf("Mr.") >= 0)
+    else if (statement.indexOf("Mr.") >= 0)
   {
    response = "He sounds like a good teacher.";
   }
-    if (statement.indexOf("Mrs.") >= 0)
+   else if (statement.indexOf("Mrs.") >= 0)
   {
    response = "She sounds like a good teacher.";
   }
-   if (statement.trim().length() == 0)
+ 
+
+  // Responses which require transformations
+  else if (findKeyword(statement, "I want to", 0) >= 0)
   {
-   response = "Say something to me!";
+   response = transformIWantToStatement(statement);
   }
-        
+  //  Part of student solution
+  else if (findKeyword(statement, "I want", 0) >= 0)
+  {
+   response = transformIWantStatement(statement);
+  }
+
+
+  else if (findKeyword(statement, "is", 0) >= 0) // if statenment has an "is" in it
+    //Move "is" to front " x is y " -> "Why is x y?"
+  {
+    response = transformIsStatement(statement);
+  }
+  
+  //ARE and no you
+    else if (findKeyword(statement, "are", 0) >=0 && findpastKeyword(statement, "you", 0) == -1)
+
+   {
+         response = transformarenoyouStatement(statement); 
+   }
+
+  
+  //ZZZ If statement has are in it, and does not contain you, 
+
+   //xxxac5 I ___ed -> Why do you ___. 
+  else if (findKeyword(statement, "I", 0) >=0 && findpastKeyword(statement, "ed", 0) >= 0)
+
+   {
+         response = transformpastStatement(statement); 
+   }
+
   else
   {
-   response = getRandomResponse();
+
+
+    //
+    
+   // Look for a two word (you <something> me)
+   // pattern
+   int psn = findKeyword(statement, "you", 0);
+
+   if (psn >= 0 && findKeyword(statement, "me", psn) >= 0)
+   {
+    response = transformYouMeStatement(statement);
+   }
+   else
+   {
+    //  Part of student solution
+    // Look for a two word (I <something> you)
+    // pattern
+    psn = findKeyword(statement, "i", 0);
+
+    if (psn >= 0
+      && findKeyword(statement, "you", psn) >= 0)
+    {
+     response = transformIYouStatement(statement);
+    }
+    
+    else
+    {
+     response = getRandomResponse();
+    }
+   }
   }
   return response;
  }
+
+//zzz Are with no you 
+ 
+  private String transformarenoyouStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfAre = findKeyword (statement, "are", 0);
+
+  String firstOfStatement = statement.substring(0, psnOfAre).trim().toLowerCase();
+  String restOfStatement = statement.substring(psnOfAre +3).trim();
+  return "Why are " + firstOfStatement + " " + restOfStatement + "?";
+ }
+ 
+ //ACTIVITY 5 There is a flat. Why is there a flat. 
+  private String transformIsStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfIs = findKeyword (statement, "is", 0);
+
+  String firstOfStatement = statement.substring(0, psnOfIs).trim().toLowerCase();
+  String restOfStatement = statement.substring(psnOfIs +3).trim();
+  return "Why is " + firstOfStatement + " " + restOfStatement + "?";
+ }
+  
+  //XXXACTIVITY 5: like "I worked." "Why did you work."
+   private String transformpastStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfI = findKeyword (statement, "I", 0);
+  int psnOfed = findpastKeyword (statement, "ed", 0);
+
+  String firstOfStatement = statement.substring( psnOfI + 2, psnOfed ).trim();
+  return "Why did you " + firstOfStatement + " ?";
+   }
+ /**
+  * Take a statement with "I want to <something>." and transform it into 
+  * "What would it mean to <something>?"
+  * @param statement the user statement, assumed to contain "I want to"
+  * @return the transformed statement
+  */
+ 
+  
+  
+ private String transformIWantToStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  int psn = findKeyword (statement, "I want to", 0);
+  String restOfStatement = statement.substring(psn + 9).trim();
+  return "What would it mean to " + restOfStatement + "?";
+ }
+
+ 
+ /**
+  * Take a statement with "I want <something>." and transform it into 
+  * "Would you really be happy if you had <something>?"
+  * @param statement the user statement, assumed to contain "I want"
+  * @return the transformed statement
+  */
+ private String transformIWantStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  int psn = findKeyword (statement, "I want", 0);
+  String restOfStatement = statement.substring(psn + 6).trim();
+  return "Would you really be happy if you had " + restOfStatement + "?";
+ }
+ 
+ /**
+  * Take a statement with "you <something> me" and transform it into 
+  * "What makes you think that I <something> you?"
+  * @param statement the user statement, assumed to contain "you" followed by "me"
+  * @return the transformed statement
+  */
+ private String transformYouMeStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfYou = findKeyword (statement, "you", 0);
+  int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+  
+  String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+  return "What makes you think that I " + restOfStatement + " you?";
+ }
+ 
+ /**
+  * Take a statement with "I <something> you" and transform it into 
+  * "Why do you <something> me?"
+  * @param statement the user statement, assumed to contain "I" followed by "you"
+  * @return the transformed statement
+  */
+ private String transformIYouStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfI = findKeyword (statement, "I", 0);
+  int psnOfYou = findKeyword (statement, "you", psnOfI);
+  
+  String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+  return "Why do you " + restOfStatement + " me?";
+ }
+ 
+
+ //XXX FIND THE __"ED" in a word! and remove the ED
+  private int findpastKeyword(String statement, String goal, int startPos)
+ {
+  String phrase = statement.trim();
+  //  The only change to incorporate the startPos is in the line below
+  int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+  
+  //  Refinement--make sure the goal isn't part of a word 
+  while (psn >= 0) 
+  {
+   //  Find the string of length 1 before and after the word
+   String before = " ", after = " "; 
+   if (psn > 0)
+   {
+    before = phrase.substring (psn, psn).toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
+   }
+   
+   //  If before and after aren't letters, we've found the word
+   if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))  //  before is not a letter
+     && ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
+   {
+    return psn;
+   }
+   
+   //  The last position didn't work, so let's find the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+   
+  }
+  
+  return -1;
+ }
+ 
+ 
+ 
+ /**
+  * Search for one word in phrase.  The search is not case sensitive.
+  * This method will check that the given goal is not a substring of a longer string
+  * (so, for example, "I know" does not contain "no").  
+  * @param statement the string to search
+  * @param goal the string to search for
+  * @param startPos the character of the string to begin the search at
+  * @return the index of the first occurrence of goal in statement or -1 if it's not found
+  */
+ private int findKeyword(String statement, String goal, int startPos)
+ {
+  String phrase = statement.trim();
+  //  The only change to incorporate the startPos is in the line below
+  int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+  
+  //  Refinement--make sure the goal isn't part of a word 
+  while (psn >= 0) 
+  {
+   //  Find the string of length 1 before and after the word
+   String before = " ", after = " "; 
+   if (psn > 0)
+   {
+    before = phrase.substring (psn - 1, psn).toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
+   }
+   
+   //  If before and after aren't letters, we've found the word
+   if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))  //  before is not a letter
+     && ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
+   {
+    return psn;
+   }
+   
+   //  The last position didn't work, so let's find the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+   
+  }
+  
+  return -1;
+ }
+ 
+ /**
+  * Search for one word in phrase.  The search is not case sensitive.
+  * This method will check that the given goal is not a substring of a longer string
+  * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
+  * @param statement the string to search
+  * @param goal the string to search for
+  * @return the index of the first occurrence of goal in statement or -1 if it's not found
+  */
+ private int findKeyword(String statement, String goal)
+ {
+  return findKeyword (statement, goal, 0);
+ }
+ 
+
 
  /**
   * Pick a default response to use if nothing else fits.
   * @return a non-committal string
   */
- private String getRandomResponse()
+ private String getRandomResponse ()
  {
-  final int NUMBER_OF_RESPONSES = 6;
-  double r = Math.random();
-  int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-  String response = "";
-  
-  if (whichResponse == 0)
-  {
-   response = "Interesting, tell me more.";
-  }
-  else if (whichResponse == 1)
-  {
-   response = "Hmmm.";
-  }
-  else if (whichResponse == 2)
-  {
-   response = "Do you really think so?";
-  }
-  else if (whichResponse == 3)
-  {
-   response = "You don't say.";
-  }
-    else if (whichResponse == 4)
-  {
-   response = "What makes you say that?";
-  }
-      else if (whichResponse == 5)
-  {
-   response = "Congratulations! You have received one of 6 randomly generated responses!";
-  }
-
-  return response;
+  Random r = new Random ();
+  return randomResponses [r.nextInt(randomResponses.length)];
  }
+ 
+ private String [] randomResponses = {"Interesting, tell me more",
+   "Hmmm.",
+   "Do you really think so?",
+   "You don't say.", 
+   "Oh really?", 
+   "How now brown cow?", 
+   "Dude, I changed my mind, I don't want to hear about your human nonsense", 
+   "42",
+   //4 random responses. 
+ };
+ 
 }
-
-// In order to prioritize responses, the reply methods would have to be arranged. 
-//Or, and and statement what would say when the sentence has "mother" and "dog" 
-//it could incorporate a reply with both, or use the last word. 
-//The problem with question1, is that it will respond with a question for vegtables
-// specifically rather than vegtables smothered in cheese, which is what the human is talking about 
-
